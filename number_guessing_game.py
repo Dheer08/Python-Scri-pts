1fb19687275da6a2a307d9ce37d092c102f5dde8
Rakshith Dheer <rakshithdheerreddy08@gmail.com>
2024-12-22 12:13:42 +0530

    guessing_game

---
+++ number_guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+print("Enter the range of numbers : ")
+range_a = int(input("Enter a staring range integer number : "))
+range_b = int(input("Enter a ending range integer number : "))
+ran_num = random.randint(range_a,range_b+1)
+print("A value has been picked by computer : ",ran_num)
+num = int(input("Guess  the number : "))
+count = 1
+
+while ran_num != num : 
+    count = count + 1
+    if num > ran_num: 
+        print("THe Guess is wrong, you Guessed too high") 
+    elif num < ran_num: 
+        print("The Guess is wrong, you Guessed too low")
+    else :
+        continue
+    num = int(input("Guess the number : "))
+
+print(f"Congratualtion! you Finally guessed the Number in {count} guesses")
---
+++ word_guessing_game.py
@@ -0,0 +1,12 @@
+import random
+
+
+words = ["Optmizer","Linear","Regression","Adam","Scalar","Vector","Matrix"]
+ran_num = random.randint(1,len(words)+1)
+ran_word = words[ran_num]
+print("random number : ",ran_num)
+print("random Word : ",ran_word)
+
+name = input("Enter your name : ")
+print("Good Luck ",name)
+

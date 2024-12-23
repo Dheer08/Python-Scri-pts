23930abb3d5a8ac56ff7ebf1b875cb81d2426948
Rakshith Dheer <rakshithdheerreddy08@gmail.com>
2024-12-23 08:30:56 +0530

    Guessing Games

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,7 +3,7 @@ import random
 print("Enter the range of numbers : ")
 range_a = int(input("Enter a staring range integer number : "))
 range_b = int(input("Enter a ending range integer number : "))
-ran_num = random.randint(range_a,range_b+1)
+ran_num = random.randint(range_a,range_b)
 print("A value has been picked by computer : ",ran_num)
 num = int(input("Guess  the number : "))
 count = 1
--- word_guessing_game.py
+++ word_guessing_game.py
@@ -2,11 +2,34 @@ import random
 
 
 words = ["Optmizer","Linear","Regression","Adam","Scalar","Vector","Matrix"]
-ran_num = random.randint(1,len(words)+1)
+ran_num = random.randint(1,len(words))
 ran_word = words[ran_num]
 print("random number : ",ran_num)
 print("random Word : ",ran_word)
-
+# ran_word = random.choice(words)
 name = input("Enter your name : ")
 print("Good Luck ",name)
+
+print("Len of word : ",len(ran_word))
+len_word = len(ran_word)
+
+turns = 12
+guess_word = ""
+
+while turns > 0:
+    fail = 0
+    inp_char = str(input("Guess a character : "))
+    if char in ran_word:
+        if inp_char in guess_word:
+            print(char,end="")
+        else:
+            print("_",end="")
+            fail = fail + 1
+    if fail == 0:
+        print("You win")
+        print(f"The Word is {ran_word}")
+        break
+    turns = turns -1
+    print(f"You have {turns} left")
+
 
